372e2f2ce216e7def828d9946a581ff51adecb58
Franco Polizzi <me@fpolizzi.com>
2026-03-23 01:49:16 +0100

    feat: add a takeWhile example

--- src/test/java/com/fpolizzi/examples/Filtering.java
+++ src/test/java/com/fpolizzi/examples/Filtering.java
@@ -39,7 +39,14 @@ public class Filtering {
 
     @Test
     public void takeWhile() throws Exception {
-        Stream.of(2, 4, 6, 8, 9, 10, 12);
+        System.out.println("using filter");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
+
+        System.out.println();
+        System.out.println("using take while");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).takeWhile(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
     }
 
     @Test
