bdd8b809824e5466f5eeba19299527d5881bd031
Franco Polizzi <me@fpolizzi.com>
2026-03-23 01:37:47 +0100

    feat: add a filter example

---
+++ src/test/java/com/fpolizzi/examples/Filtering.java
@@ -0,0 +1,62 @@
+package com.fpolizzi.examples;
+
+import com.fpolizzi.beans.Car;
+import com.fpolizzi.mockdata.MockData;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Filtering {
+
+    @Test
+    public void filter() throws Exception {
+        List<Car> cars = MockData.getCars();
+
+        Predicate<Car> carPredicate = car -> car.getPrice() < 20_000.00;
+        Predicate<Car> yellow = car -> car.getColor().equals("Yellow");
+
+        List<Car> carsLessThan20k = cars.stream()
+                .filter(carPredicate)
+                .filter(yellow)
+                .collect(Collectors.toList());
+
+        carsLessThan20k.forEach(System.out::println);
+    }
+
+    @Test
+    public void dropWhile() throws Exception {
+        Stream.of(2, 4, 6, 8, 9, 10, 12);
+    }
+
+    @Test
+    public void takeWhile() throws Exception {
+        Stream.of(2, 4, 6, 8, 9, 10, 12);
+    }
+
+    @Test
+    public void findFirst() throws Exception {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    }
+
+    @Test
+    public void findAny() throws Exception {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10};
+    }
+
+    @Test
+    public void allMatch() throws Exception {
+        int[] even = {2, 4, 6, 8, 10};
+    }
+
+    @Test
+    public void anyMatch() throws Exception {
+        int[] evenAndOneOdd = {2, 4, 6, 8, 10, 11};
+    }
+
+}
+
+
+
