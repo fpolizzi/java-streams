bb69ddcd5cdfb511efb4842d42eadb829dd60d38
Franco Polizzi <me@fpolizzi.com>
2026-03-23 01:41:28 +0100

    feat: add a dropWhile example

--- src/test/java/com/fpolizzi/examples/Filtering.java
+++ src/test/java/com/fpolizzi/examples/Filtering.java
@@ -28,7 +28,13 @@ public class Filtering {
 
     @Test
     public void dropWhile() throws Exception {
-        Stream.of(2, 4, 6, 8, 9, 10, 12);
+        System.out.println("using filter");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
+        System.out.println();
+        System.out.println("using dropWhile");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).dropWhile(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
     }
 
     @Test
