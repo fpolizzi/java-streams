df8fbeff9b371b6b8142bd3830ab6cd06a54ebb4
Franco Polizzi <me@fpolizzi.com>
2026-03-23 16:56:52 +0100

    feat: add a anyMatch example

--- src/test/java/com/fpolizzi/examples/Filtering.java
+++ src/test/java/com/fpolizzi/examples/Filtering.java
@@ -78,6 +78,8 @@ public class Filtering {
     @Test
     public void anyMatch() throws Exception {
         int[] evenAndOneOdd = {2, 4, 6, 8, 10, 11};
+        boolean anyMatch = Arrays.stream(evenAndOneOdd).anyMatch(n -> !(n % 2 == 0));
+        System.out.println(anyMatch);
     }
 
 }
