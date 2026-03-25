bf6b3f71981561b49512af31b10a05954763b6ef
Franco Polizzi <me@fpolizzi.com>
2026-03-25 23:06:19 +0100

    feat: example groupBy

---
+++ src/test/java/com/fpolizzi/examples/GroupingData.java
@@ -0,0 +1,41 @@
+package com.fpolizzi.examples;
+
+
+import com.fpolizzi.beans.Car;
+import com.fpolizzi.mockdata.MockData;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupingData {
+
+    @Test
+    public void simpleGrouping() throws Exception {
+        Map<String, List<Car>> map = MockData.getCars()
+                .stream()
+                .collect(Collectors.groupingBy(Car::getMake));
+        map.forEach((s, cars) -> {
+            System.out.println("Make " + s);
+            cars.forEach(System.out::println);
+            System.out.println("---------------------");
+        });
+    }
+
+    @Test
+    public void groupingAndCounting() throws Exception {
+        List<String> names = List.of(
+                "John",
+                "John",
+                "Mariam",
+                "Alex",
+                "Mohammado",
+                "Mohammado",
+                "Vincent",
+                "Alex",
+                "Alex"
+        );
+    }
+
+}
