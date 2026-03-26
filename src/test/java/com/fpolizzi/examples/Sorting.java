71d3e9fdc4da3df099191bb78722f49752d644ca
Franco Polizzi <me@fpolizzi.com>
2026-03-26 02:36:52 +0100

    feat: example sort steams of elements

---
+++ src/test/java/com/fpolizzi/examples/Sorting.java
@@ -0,0 +1,40 @@
+package com.fpolizzi.examples;
+
+
+import com.fpolizzi.beans.Car;
+import com.fpolizzi.beans.Person;
+import com.fpolizzi.mockdata.MockData;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Sorting {
+
+    @Test
+    public void sortingSteamOfElements() throws IOException {
+        List<Person> people = MockData.getPeople();
+
+        List<String> sorted = people.stream()
+                .map(Person::getFirstName)
+                .sorted()
+                .collect(Collectors.toList());
+        sorted.forEach(System.out::println);
+    }
+
+    @Test
+    public void sortingSteamOfElementsReverse() throws IOException {
+        List<Person> people = MockData.getPeople();
+    }
+
+    @Test
+    public void sortingSteamOfObjets() throws IOException {
+        List<Person> people = MockData.getPeople();
+    }
+
+    @Test
+    public void topTenMostExpensiveBlueCars() throws IOException {
+        List<Car> cars = MockData.getCars();
+    }
+}
