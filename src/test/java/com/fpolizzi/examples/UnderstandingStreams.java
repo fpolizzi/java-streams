4b32adb0f150fba7a3d13a8504908c20635b76c0
Franco Polizzi <me@fpolizzi.com>
2026-03-27 19:34:44 +0100

    feat: example of how streams are processed

--- src/test/java/com/fpolizzi/examples/UnderstandingStreams.java
+++ src/test/java/com/fpolizzi/examples/UnderstandingStreams.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UnderstandingStreams {
 
@@ -22,5 +23,26 @@ public class UnderstandingStreams {
                 );
 
         emails.forEach(System.out::println);
+    }
+
+    @Test
+    public void lazy() throws Exception {
+        System.out.println(
+                MockData.getCars()
+                        .stream()
+                        .filter(car -> {
+                            System.out.println("filter car " + car);
+                            return car.getPrice() < 10000;
+                        })
+                        .map(car -> {
+                            System.out.println("mapping car " + car);
+                            return car.getPrice();
+                        })
+                        .map(price -> {
+                            System.out.println("mapping price " + price);
+                            return price + (price * .14);
+                        })
+                        .collect(Collectors.toList())
+        );
     }
 }
