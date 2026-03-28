56a84cb5d2d6ab157d6858226b55320d8d84e256
Franco Polizzi <me@fpolizzi.com>
2026-03-28 23:03:12 +0100

    feat: add exercise transactionByType

---
+++ src/test/java/com/fpolizzi/exercises/ExerciseTest.java
@@ -0,0 +1,25 @@
+package com.fpolizzi.exercises;
+
+import com.fpolizzi.exercises.data.Transactions;
+import com.fpolizzi.exercises.model.Transaction;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * Created by fpolizzi on 28.03.26
+ */
+public class ExerciseTest {
+
+    private static final Transactions transactions = new Transactions();
+
+    // 01. Filter Transactions by Type
+    @Test
+    void transactionByType() {
+        List<Transaction> creditTransactions = transactions.stream()
+                .filter(transaction -> "CREDIT".equals(transaction.type()))
+                .toList();
+
+        creditTransactions.forEach(System.out::println);
+    }
+}
